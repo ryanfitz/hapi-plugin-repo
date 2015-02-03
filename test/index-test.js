068a643d031d841db1a0f445c599eb8a1102276c
Ryan Fitzgerald <ryan.fitz1@gmail.com>
2015-02-03 11:17:01 -0500

    testing passing a logger to plugin

--- test/index-test.js
+++ test/index-test.js
@@ -5,6 +5,7 @@
 var Code = require('code');
 var Hapi = require('hapi');
 var Lab = require('lab');
+var bunyan = require('bunyan');
 
 
 // Declare internals
@@ -18,18 +19,39 @@ var describe = lab.describe;
 var it = lab.it;
 var expect = Code.expect;
 
+var plugin = require('../');
 
 describe('hapi-plugin', function () {
 
   it('should not return error', function (done) {
 
     var server = new Hapi.Server();
-    server.register(require('../'), function (err) {
+    server.register(plugin, function (err) {
 
       expect(err).to.not.exist();
 
       return done();
     });
   });
+
+  it('should create child logger', function (done) {
+
+    var server = new Hapi.Server();
+
+    var options = {
+      log : bunyan.createLogger({name: 'plugin-test', level : 'fatal'})
+    };
+
+    server.register( {
+      register : plugin,
+      options : options
+    }, function (err) {
+
+      expect(err).to.not.exist();
+
+      return done();
+    });
+  });
+
 
 });
